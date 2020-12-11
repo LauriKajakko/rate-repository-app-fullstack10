b0ad8776771b406d12e3df14973f7fdfdb3272b4
LauriKajakko <kajakko.lauri@gmail.com>
2020-12-11 17:01:58 +0200

    small modifications

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import Constants from 'expo-constants';
 import { StyleSheet, View } from 'react-native';
-import Text from './Text';
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: Constants.statusBarHeight,
     flexGrow: 1,
     flexShrink: 1,
   },
@@ -16,11 +13,10 @@ const styles = StyleSheet.create({
 const Main = () => {
   return (
     <View>
-    <AppBar/>
-    <View style={styles.container}>
-      <Text color='primary'>Rate Repository Application</Text>
-      <RepositoryList />
-    </View>
+      <AppBar/>
+      <View style={styles.container}>
+        <RepositoryList />
+      </View>
     </View> 
   );
 };
